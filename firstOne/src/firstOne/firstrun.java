631f1df88b5fb6ac8750b37b43a2f509b66bb4d9
insidehome <insidehome@insidehome-X200T>
2015-05-09 19:51:53 +0800

    20150509

---
+++ firstOne/src/firstOne/firstrun.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package firstOne;
+
+/**
+ * @author insidehome
+ *
+ */
+public class firstrun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World. This is the firstrun.");
+	}
+
+}
